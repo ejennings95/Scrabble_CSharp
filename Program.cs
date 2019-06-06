32a7770d31f40dc412ceb3e60959ed2d34d00f93
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-06-06 16:17:15 +0100

    :see_no_evil: :memo: initial setup - README and .gitignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Scrabble
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
