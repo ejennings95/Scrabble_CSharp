c97ec4d9753edfc01bf45c1f7b6d82a4941a4fcc
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-06-07 11:56:13 +0100

    :sparkles: LetterScore returns correct points for letter. Tests passing.

---
+++ Properties/Tests/LetterScoreTest.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Scrabble.Main;
+using System;
+namespace Scrabble.Properties.Tests
+{
+    public class LetterScoreTest
+    {
+        [Test, Description("A should return letterscore 1")]
+        public void GetAPoints()
+        {
+            LetterScore letter = new LetterScore('A');
+            Assert.AreEqual(1, letter.GetLetterScore());
+        }
+
+        [Test, Description("F should return letterscore 4")]
+        public void GetFPoints()
+        {
+            LetterScore letter = new LetterScore('F');
+            Assert.AreEqual(4, letter.GetLetterScore());
+        }
+
+        [Test, Description("Z should return letterscore 10")]
+        public void GetZPoints()
+        {
+            LetterScore letter = new LetterScore('Z');
+            Assert.AreEqual(10, letter.GetLetterScore());
+        }
+    }
+}
